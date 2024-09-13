88f11fd6a3fe50a64623e27284250faff44bfa29
Gregory Marchenkov <wowg195@gmail.com>
2024-09-13 12:16:58 +0300

    remove ScrollView from ForgotPassword screen

--- src/modules/forgot-password/index.tsx
+++ src/modules/forgot-password/index.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-import {SafeAreaView, ScrollView, View} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {Button, Layout, useStyleSheet, Text, Icon} from '@ui-kitten/components';
 import {FormProvider} from 'react-hook-form';
 
@@ -16,51 +16,49 @@ const ForgotPassword: FC = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <View>
-          <View style={styles.headerContainer}>
-            <Text category="h1" status="control">
-              bB
-            </Text>
-            <Text style={styles.headerLabel} category="s1" status="control">
-              Forgot Password
-            </Text>
-          </View>
-
-          <FormProvider {...formData}>
-            <Layout style={styles.formContainer} level="1">
-              <Text style={styles.emailLabel}>
-                Please enter your email address
-              </Text>
-
-              <Input
-                name="email"
-                style={styles.emailInput}
-                autoCapitalize="none"
-                placeholder="Email"
-                accessoryRight={<Icon name="email" />}
-              />
-            </Layout>
-          </FormProvider>
+      <View>
+        <View style={styles.headerContainer}>
+          <Text category="h1" status="control">
+            bB
+          </Text>
+          <Text style={styles.headerLabel} category="s1" status="control">
+            Forgot Password
+          </Text>
         </View>
 
-        <View>
-          <Button
-            style={styles.submitButton}
-            size="giant"
-            onPress={submitHandler}>
-            SEND VERIFICATION CODE
-          </Button>
+        <FormProvider {...formData}>
+          <Layout style={styles.formContainer} level="1">
+            <Text style={styles.emailLabel}>
+              Please enter your email address
+            </Text>
 
-          <Button
-            style={styles.goBackButton}
-            appearance="ghost"
-            status="basic"
-            onPress={goBack}>
-            Go back
-          </Button>
-        </View>
-      </ScrollView>
+            <Input
+              name="email"
+              style={styles.emailInput}
+              autoCapitalize="none"
+              placeholder="Email"
+              accessoryRight={<Icon name="email" />}
+            />
+          </Layout>
+        </FormProvider>
+      </View>
+
+      <View>
+        <Button
+          style={styles.submitButton}
+          size="giant"
+          onPress={submitHandler}>
+          SEND VERIFICATION CODE
+        </Button>
+
+        <Button
+          style={styles.goBackButton}
+          appearance="ghost"
+          status="basic"
+          onPress={goBack}>
+          Go back
+        </Button>
+      </View>
     </SafeAreaView>
   );
 };
